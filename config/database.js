1dcefe878a2a405b331d51d17d935f3fd40bcc60
Pranat1 <74774569+Pranat1@users.noreply.github.com>
2022-06-01 18:48:44 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -11,6 +11,7 @@ const path = require('path');
 // });
 
 
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -18,11 +19,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', 'ec2-54-165-178-178.compute-1.amazonaws.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''), 
+        database: env('DATABASE_NAME', 'd4d2m9bvdi8gmo'),
+        username: env('DATABASE_USERNAME', 'mgochqsrhrftcl'),
+        password: env('DATABASE_PASSWORD', 'd15652676187cb8daddcb5cf26bfee0824652bd53220675d2241c590ff58f8c4'), 
         //add this line
         ssl: {
           rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
