e304192f791e84abe93ba7e9509960dbea3d96c2
Pranat1 <74774569+Pranat1@users.noreply.github.com>
2022-06-01 16:46:02 +0530

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','r9BJZjRTI9/agoYMOFwZ2Q=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','r9BJZjRTI9/agoYMOFwZ2Q==r9BJZjRTI9/agoYMOFwZ2Q=='),
   },
 });
